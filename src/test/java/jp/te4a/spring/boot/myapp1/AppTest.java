bdc91d71cf463f9d2b798fabda6959bab5298cee
minoru-oya <83932690+minoru-oya@users.noreply.github.com>
2021-09-29 09:55:42 +0900

    testコードの変更

--- src/test/java/jp/te4a/spring/boot/myapp1/AppTest.java
+++ src/test/java/jp/te4a/spring/boot/myapp1/AppTest.java
@@ -17,6 +17,15 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+    @Test
+	public void test() {
+		HelloController helloController = new HelloController();
+		String expected = "Hello, Spring Boot!";
+		String actual = helloController.index();
+		
+		assertEquals(expected,actual);
+	}
     
     
 }
